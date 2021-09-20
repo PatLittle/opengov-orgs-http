c9de076967f906a152c37188a8fe1fcc34d3ec01
HoussamBedja <HoussamBedja@users.noreply.github.com>
2021-09-20 13:06:15 +0000

    deploy: 7b6c720f0fa9d694006a46e505364eecb84907df

--- load_org_details.js
+++ load_org_details.js
@@ -7,7 +7,7 @@ function getDataForDatatables(){
 var url = window.location.href;
 var org_id = decodeURIComponent(url.split("?").pop());
 var org_name = org_id.split('|')[0].trim();
-$('h2').html('List of <i>"http"</i> and <i>"ftp"</i> urls: ' + org_name)
+$('h1').html(org_name)
 
 $.getJSON( "orgs_http_data.json", function( data ) {
 setDataToTable(data[org_id]);
@@ -15,39 +15,41 @@ setDataToTable(data[org_id]);
 }
 
 function setDataToTable(jsonData){
-$('#employee').DataTable( {
+$('#org-details').DataTable( {
   "autoWidth": false,
-pagination: "bootstrap",
+  pagination: "bootstrap",
   filter:true,
   data: jsonData,
-  destroy: true,
+  "scrollX": true,
   pageLength: 25,
   "columns":[  
-    {"data": "dataset"},
-    {"data": "url"},
-    {"data": "url_type"},
-    {"data": "dataset_id"},
-    {"data": "id"},
+    {"data": "dataset", width: '20%'},
+    {"data": "url", width: '50%'},
+    {"data": "url_type", width: '5%'},
+    {"data": "dataset_id", width: '15%'},
+    {"data": "id", width: '10%'},
   ],
   columnDefs: [
-  { "width": "25%", "targets": 0 },
-  { "width": "40%", "targets": 1 },
-        {
-        targets: 3,
-        render: function (data, type, row, meta)
-        {
+    {"className": "dt-center", "targets": [2,3,4] },
+    {
+    targets: 3,
+    render: function (data, type, row, meta)
+    {
 
-            data = '<a target="_blank" href="https://open.canada.ca/data/en/dataset/' + data + '">View dataset</a>';
-            return data;
-        }
-        },
-        {
-        targets: 4,
-        render: function (data, type, row, meta)
-        {
-            data = '<a target="_blank" href="https://registry.open.canada.ca/en/dataset/' + row.dataset_id + '/resource_edit/' + data + '">Edit resource</a>';
-            return data;
-        }
-        }],
+      data = '<a target="_blank" href="https://open.canada.ca/data/en/dataset/' + data + '">View dataset</a>';
+      return data;
+    }
+    },
+    {
+    targets: 4,
+    render: function (data, type, row, meta)
+    {
+      if (row.from_registry) {
+        return '<a target="_blank" class="btn btn-primary" href="https://registry.open.canada.ca/en/dataset/' + row.dataset_id + '/resource_edit/' + data + '">Edit resource</a>';
+      }
+      return 'N/A';
+    }
+    }
+  ],
 } );
 }
